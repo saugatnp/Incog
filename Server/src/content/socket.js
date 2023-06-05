dbd2c9a8a8ea88e6bcd3695243228d08ba9c73cb
saugatnp <saugatnp11@gmail.com>
2023-06-06 00:51:59 +0545

    room connection and message sending functionality added

--- Server/src/content/socket.js
+++ Server/src/content/socket.js
@@ -44,5 +44,175 @@ export const c = (query) => {
 };
 
 
+export const d = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const e = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const g = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gh= (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+export const fi = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gj = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+export const fk = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gl = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+export const fm = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gn = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+export const fo = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gp = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+export const fq = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gr = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+export const fw = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
+export const gt = (query) => {
+
+
+    return io('http://your-socket-server-url', {
+        query: query
+    });
+
+
+};
+
+
 
 export const socket = createSocket(null);
