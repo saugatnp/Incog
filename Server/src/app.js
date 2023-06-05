c07ea5d6ea7f32e4d28dfadb530c38d815e851a5
saugatnp <saugatnp11@gmail.com>
2023-06-06 00:36:34 +0545

    room connection and message sending functionality added

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { join } = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const Server = require("socket.io");
 const io = new Server(http);
+
+var rooms = [];
+var clientList = [];
 
 
 
@@ -19,22 +23,46 @@ app.get('/chat', (req, res) => {
 
 
 io.on('connection', (socket) => {
+  socket.joinRooms = [];
+  clientList.push(socket.id);
+  socket.on('find-room', (msg) => {
+    var roomId = []
+    if (rooms.length == 0 || rooms.every(x => x.users.length == 2)) {
+       roomId.id = Math.floor(Math.random() * 1000000);
+      rooms.push({ id: roomId.id, users: [socket.id] });
+      socket.join(roomId.id);
+      socket.joinRooms.push(roomId.id);
+    }
+    else {
+      var roomId = rooms.find(x => x.users.length != 2);
+      socket.join(roomId.id);
+      rooms.find(x => x.id == roomId.id).users.push(socket.id);
+    }
+    if(rooms.find(x => x.id == roomId.id).users.length == 2)
+      io.to(roomId.id).emit('connected', { 'roomId': roomId.id, "message": "User connected" });
+  });
+
+  
+
   socket.on('message', (msg) => {
-    console.log(msg);
-    io.emit('message', { 'username': socket.username, "message": msg.msg });
+    socket.broadcast.to(msg.roomId).emit('message', { 'username': socket.handshake.query.token, "message": msg.msg });
+  });
+  socket.on('disconnect', () => {
+    clientList.splice(clientList.indexOf(socket.id), 1);
+    var disconnectedRoom = rooms.filter(x => socket.joinRooms.includes(x.id));
+    if (disconnectedRoom.length > 0)
+      io.to(disconnectedRoom[0].id).emit('disconnect', { 'username': "Server", "message": "User disconnected" });
+
   });
 });
 
 
 
-
 io.use((socket, next) => {
   try {
-    console.log("middleware connection")
     if (!socket.handshake.query.token) {
       return next(new Error("invalid username"));
     }
-    socket.username = socket.handshake.query.token;
     socket.emit('redirect', 'http://localhost:3000/chat');
 
     next();
