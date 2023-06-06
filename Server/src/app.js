6e8d55b84012e279eb784edca779da608e4e58f5
saugatnp <saugatnp11@gmail.com>
2023-06-07 00:33:10 +0545

    user disconnected emittion added

--- Server/src/app.js
+++ Server/src/app.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/content/register.html');
 });
 
+app.get('/demo', (req, res) => {
+  res.sendFile(__dirname + '/content/chat.html');
+});
+
 
 
 app.get('/chat', (req, res) => {
@@ -44,7 +48,7 @@ io.on('connection', (socket) => {
   socket.on('find-room', (msg) => {
     var roomId = []
     if (rooms.length == 0 || rooms.every(x => x.users.length == 2)) {
-       roomId.id = Math.floor(Math.random() * 1000000);
+      roomId.id = Math.floor(Math.random() * 1000000);
       rooms.push({ id: roomId.id, users: [socket.id] });
       socket.join(roomId.id);
       socket.joinRooms.push(roomId.id);
@@ -52,13 +56,14 @@ io.on('connection', (socket) => {
     else {
       var roomId = rooms.find(x => x.users.length != 2);
       socket.join(roomId.id);
+      socket.joinRooms.push(roomId.id);
       rooms.find(x => x.id == roomId.id).users.push(socket.id);
     }
-    if(rooms.find(x => x.id == roomId.id).users.length == 2)
+    if (rooms.find(x => x.id == roomId.id).users.length == 2)
       io.to(roomId.id).emit('connected', { 'roomId': roomId.id, "message": "User connected" });
   });
 
-  
+
 
   socket.on('message', (msg) => {
     socket.broadcast.to(msg.roomId).emit('message', { 'username': socket.handshake.query.token, "message": msg.msg });
@@ -66,10 +71,18 @@ io.on('connection', (socket) => {
 
 
   socket.on('disconnect', () => {
-    clientList.splice(clientList.indexOf(socket.id), 1);
-    var disconnectedRoom = rooms.filter(x => socket.joinRooms.includes(x.id));
-    if (disconnectedRoom.length > 0)
-      io.to(disconnectedRoom[0].id).emit('disconnect', { 'username': "Server", "message": "User disconnected" });
+    const { type } = socket.handshake.query;
+    if (type !== "register") {
+      clientList.splice(clientList.indexOf(socket.id), 1);
+      var disconnectedRoom = rooms.filter(x => socket.joinRooms.includes(x.id));
+      rooms.splice(rooms.indexOf(disconnectedRoom.users), 1);
+      if (disconnectedRoom.length > 0)
+        io.to(disconnectedRoom[0].id).emit('disconnect', { 'username': "Server", "message": "User disconnected" });
+    }
+    else {
+      clientList.splice(clientList.indexOf(socket.id), 1);
+
+    }
   });
 });
 
